14420b9300aa74b559b689165e9138d89dd9f502
SuperLiangi <675184354@qq.com>
2020-03-20 21:04:54 +0800

    学习Springboot01上传到github

---
+++ src/main/java/com/liang/HelloWorldMainApplication.java
@@ -0,0 +1,13 @@
+package com.liang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @SpringBootApplication 来标注一个主程序类，说明这是一个Spring Boot应用
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
